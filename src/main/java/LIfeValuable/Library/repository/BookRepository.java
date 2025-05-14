48559bf3dbc3193fa9937d1eecd43b82519ee1a5
LifeValuable <nikish0715@yandex.ru>
2025-05-14 13:33:38 +0300

    [repository] поправлен запрос в BookRepository

--- src/main/java/LIfeValuable/Library/repository/BookRepository.java
+++ src/main/java/LIfeValuable/Library/repository/BookRepository.java
@@ -19,8 +19,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByAuthor(String author, Pageable pageable);
     Optional<Book> findByIsbn(String isbn);
     Page<Book> findByPublicationYear(Integer year, Pageable pageable);
-    @Query("SELECT b FROM Book b JOIN b.genres g WHERE g IN :genres GROUP BY b HAVING COUNT(DISTINCT g) = :genresCount")
-    Page<Book> findByAllGenres(@Param("genres") List<Genre> genres, @Param("genresCount") Long genresCount, Pageable pageable);
+    @Query("SELECT b FROM Book b JOIN b.genres g WHERE g IN :genres GROUP BY b HAVING COUNT(DISTINCT g) = :#{#genres.size()}")
+    Page<Book> findByAllGenres(@Param("genres") List<Genre> genres, Pageable pageable);
+
 
     Page<Book> findByPublicationYearBetween(Integer start, Integer end, Pageable pageable);
     Page<Book> findByStockGreaterThan(Integer min, Pageable pageable);
